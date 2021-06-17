0cd49184263264819bc74a53ddea0e65dd28c0ed
Marco Silva <marcosilva.tec@gamil.com>
2021-06-17 12:54:53 -0300

    UPDATE

--- src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
@@ -47,6 +48,19 @@ public class EmployeeController {
 
 	}
 
+	@GetMapping("/showFormForUpdate")
+	public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+
+		// get the employee from the service
+		Employee theEmployee = employeeService.findById(theId);
+
+		// set employee as a model attribute to pre-populate the form
+		theModel.addAttribute("employee", theEmployee);
+
+		// send over to our form
+		return "employees/employee-form";
+	}
+
 	@PostMapping("/save")
 	public String saveEmployee(@ModelAttribute("employee") Employee theEmployee) {
 
@@ -57,4 +71,5 @@ public class EmployeeController {
 		return "redirect:/employees/list";
 
 	}
+
 }
